064d524224743c7f9af23ae93b54e29ba65bf286
deepak raghavendra <deepakraghavendra@gmail.com>
2022-10-25 23:43:04 +0530

    Webservice - Salary Data
    Demo - Expose Salary Data flow as WS using FastAPI.

--- SimpleLinearRegression/main.py
+++ SimpleLinearRegression/main.py
@@ -1,28 +1,41 @@
+import numpy as np
 import pandas as pd
 import pickle
-
-
-
-
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from fastapi import FastAPI
 
-dataSet = pd.read_csv("dataset/Salary_Data.csv")
-X = dataSet.iloc[:,:-1]
-y = dataSet.iloc[:,-1]
+app = FastAPI()
 
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=1/5)
 
-model = LinearRegression()
-model.fit(X_train,y_train)
 
-y_pred = model.predict(X_test)
 
-print("X_test",X_test)
-print("y_pred",y_pred)
+@app.get("/main/{years_exp}")
+def index(years_exp: float):
+    dataSet = pd.read_csv("dataset/Salary_Data.csv")
+    X = dataSet.iloc[:,:-1]
+    y = dataSet.iloc[:,-1]
 
-fileName = 'SalaryPredictionModel'
-pickle.dump(model,open(fileName,'wb'))
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=1/5)
 
-loaded_model = pickle.load(open(fileName,'rb'))
-loaded_model.predict(X_test)
+    model = LinearRegression()
+    model.fit(X_train,y_train)
+
+    y_pred = model.predict(X_test)
+
+    sample_input = [[years_exp]]
+
+    sample_pred = model.predict(sample_input)
+
+# print("X_test",X_test)
+# print("y_pred",y_pred)
+# print("sample_pred:",round(sample_pred[0],2))
+
+
+    return {"value":round(sample_pred[0],2)}
+
+# fileName = 'SalaryPredictionModel'
+# pickle.dump(model,open(fileName,'wb'))
+#
+# loaded_model = pickle.load(open(fileName,'rb'))
+# loaded_model.predict(X_test)
